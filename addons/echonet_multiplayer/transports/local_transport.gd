f3053fb9a937c5022a47396c5ebc865e5d90407d
Joaquin C <joaquinfcuellar@gmail.com>
2025-07-29 15:42:34 -0600

    feat LocalTransport implemented
    feat LocalTransport implemented

--- addons/echonet_multiplayer/transports/local_transport.gd
+++ addons/echonet_multiplayer/transports/local_transport.gd
@@ -1,2 +1,19 @@
 ## Transport for running local simulations without need for major code restructuring
 class_name LocalTransport extends EchonetTransport
+
+func init_server() -> bool:
+	if !super.init_server(): return false
+	_connection_successful = true
+	return true
+
+func init_client() -> bool:
+	push_warning("Cannot call 'init_client' using LocalTransport")
+	return false
+
+func init_server_info_request() -> bool:
+	push_warning("Cannot call 'init_server_info_request' using LocalTransport")
+	return false
+
+func init_headless_server() -> bool:
+	push_warning("Cannot call 'init_headless_server' using LocalTransport")
+	return false
