6e9f9ba953042fca7ccf047c8bddcb15075ddcf8
Joaquin C <joaquinfcuellar@gmail.com>
2025-08-06 09:49:24 -0600

    removed excess EchoNode vars
    removed excess EchoNode vars

--- addons/echonet_multiplayer/nodes/echo_node.gd
+++ addons/echonet_multiplayer/nodes/echo_node.gd
@@ -1,19 +1,13 @@
 class_name EchoNode extends Node
 
-var parent_id: int
-var parent_node: Node
 var id: int
 var parent_echo_scene: EchoScene
 
 func _enter_tree() -> void:
 	parent_echo_scene = _find_parent_echo_scene()
-	if parent_echo_scene != null:
-		parent_id = parent_echo_scene.id
-		parent_node = parent_echo_scene.node
-		id = parent_echo_scene.get_available_echo_node_id()
-		parent_echo_scene.echo_nodes[id] = self
-		print("New EchoNode (%s)(%s)(%s)"%[parent_node, parent_id, id])
-	else: push_warning("EchoNode should have a parent EchoScene!")
+	assert(parent_echo_scene != null, "EchoNode entered tree with no parent EchoScene!")
+	id = parent_echo_scene.get_available_echo_node_id()
+	parent_echo_scene.echo_nodes[id] = self
 
 func _exit_tree() -> void:
 	if parent_echo_scene != null && parent_echo_scene.echo_nodes.has(id):
