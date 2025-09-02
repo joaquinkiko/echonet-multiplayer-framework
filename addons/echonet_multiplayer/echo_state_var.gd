398f52e6bdee8a056e33186e8f91ad4fa28e0d25
Joaquin C <joaquinfcuellar@gmail.com>
2025-09-02 09:32:57 -0600

    Simplified client prediction
    Simplified client prediction

--- addons/echonet_multiplayer/echo_state_var.gd
+++ addons/echonet_multiplayer/echo_state_var.gd
@@ -3,48 +3,44 @@ class_name EchoStateVar extends EchoVar
 
 @export var allowed_discrepency: float
 @export_range(0.1, 1.0, .05) var interpolate_strength: float = 0.5
-@export var client_prediction_allowence: float
-var _original_allowed_discrepency: float
-
-func _init() -> void:
-	_original_allowed_discrepency = allowed_discrepency
+var _adjusted_allowed_discrepency: float
 
 func set_var(source: Node, value: Variant) -> void:
-	if client_prediction_allowence != 0:
+	if allowed_discrepency != 0:
 		var tick_latency: int = Echonet.transport.get_server_latency() / Echonet.transport.msec_per_tick
-		allowed_discrepency = _original_allowed_discrepency + client_prediction_allowence * tick_latency
+		_adjusted_allowed_discrepency = allowed_discrepency + allowed_discrepency * tick_latency
 	if source.parent_echo_scene.owner.is_self:
 		var source_value: Variant = get_var(source)
 		match typeof(source_value):
 			TYPE_INT:
-				if abs(source_value - value) <= allowed_discrepency:
+				if abs(source_value - value) <= _adjusted_allowed_discrepency:
 					return
 			TYPE_FLOAT:
-				if abs(source_value - value) <= allowed_discrepency:
+				if abs(source_value - value) <= _adjusted_allowed_discrepency:
 					return
 			TYPE_VECTOR2:
-				if abs(source_value.x - value.x) <= allowed_discrepency:
+				if abs(source_value.x - value.x) <= _adjusted_allowed_discrepency:
 					return
-				elif abs(source_value.y - value.y) <= allowed_discrepency:
+				elif abs(source_value.y - value.y) <= _adjusted_allowed_discrepency:
 					return
 			TYPE_VECTOR3:
-				if abs(source_value.x - value.x) <= allowed_discrepency:
+				if abs(source_value.x - value.x) <= _adjusted_allowed_discrepency:
 					return
-				elif abs(source_value.y - value.y) <= allowed_discrepency:
+				elif abs(source_value.y - value.y) <= _adjusted_allowed_discrepency:
 					return
-				elif abs(source_value.z - value.z) <= allowed_discrepency:
+				elif abs(source_value.z - value.z) <= _adjusted_allowed_discrepency:
 					return
 			TYPE_VECTOR2I:
-				if abs(source_value.x - value.x) <= allowed_discrepency:
+				if abs(source_value.x - value.x) <= _adjusted_allowed_discrepency:
 					return
-				elif abs(source_value.y - value.y) <= allowed_discrepency:
+				elif abs(source_value.y - value.y) <= _adjusted_allowed_discrepency:
 					return
 			TYPE_VECTOR3I:
-				if abs(source_value.x - value.x) <= allowed_discrepency:
+				if abs(source_value.x - value.x) <= _adjusted_allowed_discrepency:
 					return
-				elif abs(source_value.y - value.y) <= allowed_discrepency:
+				elif abs(source_value.y - value.y) <= _adjusted_allowed_discrepency:
 					return
-				elif abs(source_value.z - value.z) <= allowed_discrepency:
+				elif abs(source_value.z - value.z) <= _adjusted_allowed_discrepency:
 					return
 	if interpolate_strength != 1:
 		value = lerp(get_var(source), value, interpolate_strength)
