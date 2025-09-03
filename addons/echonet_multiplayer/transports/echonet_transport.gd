09de504843e1997c9a074cf3369765a0b752e1b8
Joaquin C <joaquinfcuellar@gmail.com>
2025-09-03 09:36:05 -0600

    Set max_stored_snapshots to vary with tick rate

--- addons/echonet_multiplayer/transports/echonet_transport.gd
+++ addons/echonet_multiplayer/transports/echonet_transport.gd
@@ -256,7 +256,9 @@ var last_received_snapshot_tick: int
 ## 8-bit Flag for each [EchoSnapshot] received prior to [member last_received_snapshot_tick]
 var old_received_snapshots_flags: int
 ## Limit on stored snapshots
-var max_stored_snapshots: int = 120
+var max_stored_snapshots: int:
+	get: return (MAX_TIME_DESYNC / msec_per_tick) * 1.5
+	set(value): push_warning("Cannot directly set 'max_stored_snapshots'")
 ## Stored delta [EchoSnapshot] of past ticks sent to clients
 var stored_delta_snapshots: Dictionary[int, Dictionary]
 ## Acknowledged ticks per client
